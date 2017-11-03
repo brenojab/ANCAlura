9c24ae9950ea5be5b4a7c9f0e00980625b3c7ee3
Breno Batista <brenojab@gmail.com>
2017-11-03 20:21:25 -0200

    - Loja da "Casa do Código" - Aprendizado sobre o uso do bootstrap com o "<bs-" + Tab + Tab.

---
+++ CasaCodigoVS/CasaCodigoVS/Controllers/PedidoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CasaCodigoVS.Controllers
+{
+  public class PedidoController : Controller
+  {
+    public IActionResult Carrossel()
+    {
+      return View();
+    }
+
+    public IActionResult Carrinho()
+    {
+      return View();
+
+    }
+
+    public IActionResult Resumo()
+    {
+      return View();
+
+    }
+  }
+}
