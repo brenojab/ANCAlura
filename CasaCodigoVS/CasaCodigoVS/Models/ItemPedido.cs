87a4da6784892ade1aee85c9016839bf45241833
Breno Batista <brenojab@gmail.com>
2017-11-05 16:14:47 -0200

    - Resolvendo os campos de valores e quantidades das views.

---
+++ CasaCodigoVS/CasaCodigoVS/Models/ItemPedido.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CasaCodigoVS.Models
+{
+  public class ItemPedido
+  {
+    public int Id { get; private set; }
+    public Produto Produto { get; private set; }
+    public int Quantidade { get; private set; }
+    public decimal PrecoUnitario { get; private set; }
+    public decimal Subtotal
+    {
+      get
+      {
+        return Quantidade * PrecoUnitario;
+      }
+    }
+
+    public ItemPedido(int id, Produto produto, int quantidade, decimal precoUnitario)
+    {
+      Id = id;
+      Produto = produto;
+      Quantidade = quantidade;
+      PrecoUnitario = precoUnitario;
+    }
+  }
+}
--- CasaCodigoVS/CasaCodigoVS/Models/Produto.cs
+++ CasaCodigoVS/CasaCodigoVS/Models/Produto.cs
@@ -5,10 +5,17 @@ using System.Threading.Tasks;
 
 namespace CasaCodigoVS.Models
 {
-  public class Produtos
+  public class Produto
   {
     public int Id { get; set; }
     public string Nome { get; set; }
     public decimal Preco { get; set; }
+
+    public Produto(int id, string nome, decimal preco)
+    {
+      this.Id = id;
+      this.Nome = nome;
+      this.Preco = preco;
+    }
   }
 }
