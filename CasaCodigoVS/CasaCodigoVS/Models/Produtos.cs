7404850258e0f9a985d327ec86580fc9a12fc460
Breno Batista <brenojab@gmail.com>
2017-11-05 15:30:33 -0200

    - Alterações para efetuar repetições no Razor. - Corrigido o problema dos produtos serem exibidos todos de uma só vez (era necessário fechar a primeira div). - Atribuindo à view um array de produtos como objetos.

---
+++ CasaCodigoVS/CasaCodigoVS/Models/Produtos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaCodigoVS.Models
+{
+  public class Produtos
+  {
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public decimal Preco { get; set; }
+  }
+}
